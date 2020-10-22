1ffa031a746a55a5ff952636ee8c416e471e2385
Andy Wang <yagosys@gmail.com>
2020-10-22 21:53:39 +0800

    first commit

--- main.go
+++ main.go
@@ -2,18 +2,25 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/pulumi/pulumi-alicloud/sdk/v2/go/alicloud"
 	"github.com/pulumi/pulumi-alicloud/sdk/v2/go/alicloud/ecs"
 	"github.com/pulumi/pulumi-alicloud/sdk/v2/go/alicloud/vpc"
+	"github.com/pulumi/pulumi-alicloud/sdk/v2/go/alicloud/marketplace"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
 type infrastructure struct {
 	vpcnetwork        *vpc.Network
-	vswitch           *vpc.Switch
+	vswitchpub        *vpc.Switch
+	vswitchpri1       *vpc.Switch
 	securitygroup     *ecs.SecurityGroup
 	fortigateinstance *ecs.Instance
 	sgrule            *ecs.SecurityGroupRule
+	eni               *vpc.NetworkInterface
+	rtpri1            *vpc.RouteTable
+	eniAtt            *vpc.NetworkInterfaceAttachment
 }
 
 func createInfrastructure(ctx *pulumi.Context) (*infrastructure, error) {
@@ -21,9 +28,35 @@ func createInfrastructure(ctx *pulumi.Context) (*infrastructure, error) {
 	description := "asfasdf jalsdfj alsdfj alsf asdf asfjaksfj kasf "
 	name := "vpc_name_test"
 	cidr := "192.168.0.0/16"
-	vswitch1_cidr := "192.168.0.0/24"
+	vswitchpub1_cidr := "192.168.0.0/24"
+	vswitchpri1_cidr := "192.168.1.0/24"
 	myinstanceType := "ecs.s6-c1m2.small"
-	fortigate622ImageId := "m-2zeikw0lpc9k4fubwplw"
+	fortigate624trial := "cmjj00040535"
+	//fortigate622ImageId := "m-8vb5o2t7kujhxgnm5tyu"
+	
+	opt0 := true
+        currentRegionDs, err := alicloud.GetRegions(ctx, &alicloud.GetRegionsArgs{
+        	Current: &opt0,
+        })
+
+       	if err != nil {
+        	return nil,err
+        }
+
+	defaultRegionId:=currentRegionDs.Regions[0].Id
+
+
+	_default, err := marketplace.GetProduct(ctx, &marketplace.GetProductArgs{
+            ProductCode: fortigate624trial,
+	    AvailableRegion: &defaultRegionId,
+    	})
+
+	if err !=nil {
+		return nil,err
+	}
+	fortigate622ImageId :=_default.Products[0].Skuses[0].Images[0].ImageId
+	fmt.Println(fortigate622ImageId)
+
 
 	vpcArgs := &vpc.NetworkArgs{
 		CidrBlock:   pulumi.String(cidr),
@@ -33,7 +66,7 @@ func createInfrastructure(ctx *pulumi.Context) (*infrastructure, error) {
 
 	myvpc, err := vpc.NewNetwork(ctx, "vpc-1", vpcArgs)
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
 
 	zonesDs, err := alicloud.GetZones(ctx, &alicloud.GetZonesArgs{
@@ -41,7 +74,7 @@ func createInfrastructure(ctx *pulumi.Context) (*infrastructure, error) {
 	})
 
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
 
 	az1 := zonesDs.Zones[0].Id
@@ -50,14 +83,24 @@ func createInfrastructure(ctx *pulumi.Context) (*infrastructure, error) {
 		az1 = "cn-beijing-b"
 	}
 
-	vswitch, err := vpc.NewSwitch(ctx, "switch-1", &vpc.SwitchArgs{
+	vswitchpub, err := vpc.NewSwitch(ctx, "switch-1", &vpc.SwitchArgs{
 		VpcId:            myvpc.ID(),
 		AvailabilityZone: pulumi.String(az1),
-		CidrBlock:        pulumi.String(vswitch1_cidr),
+		CidrBlock:        pulumi.String(vswitchpub1_cidr),
 	})
 
 	if err != nil {
-		return nil,err
+		return nil, err
+	}
+
+	vswitchpri1, err := vpc.NewSwitch(ctx, "switch-private-1", &vpc.SwitchArgs{
+		VpcId:            myvpc.ID(),
+		AvailabilityZone: pulumi.String(az1),
+		CidrBlock:        pulumi.String(vswitchpri1_cidr),
+	})
+
+	if err != nil {
+		return nil, err
 	}
 
 	group, err := ecs.NewSecurityGroup(ctx, "web-secgrp", &ecs.SecurityGroupArgs{
@@ -65,7 +108,7 @@ func createInfrastructure(ctx *pulumi.Context) (*infrastructure, error) {
 	})
 
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
 
 	sgrule1, err := ecs.NewSecurityGroupRule(ctx, "sg-rule1", &ecs.SecurityGroupRuleArgs{
@@ -77,31 +120,80 @@ func createInfrastructure(ctx *pulumi.Context) (*infrastructure, error) {
 	})
 
 	if err != nil {
-		return nil,err
+		return nil, err
+	}
+
+	eni1, err := vpc.NewNetworkInterface(ctx, "firstSecondarylyNetworkInterface", &vpc.NetworkInterfaceArgs{
+		VswitchId: vswitchpri1.ID(),
+		SecurityGroups: pulumi.StringArray{
+			group.ID(),
+		},
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	rtpri1, err := vpc.NewRouteTable(ctx, "routetableforprivatenetwork", &vpc.RouteTableArgs{
+		VpcId: myvpc.ID(),
+	})
+	if err != nil {
+		return nil, err
+	}
+	_, err = vpc.NewRouteTableAttachment(ctx, "routetableattachtovswithpri1", &vpc.RouteTableAttachmentArgs{
+		RouteTableId: rtpri1.ID(),
+		VswitchId:    vswitchpri1.ID(),
+	})
+	if err != nil {
+		return nil, err
 	}
 
 	myinstance, err := ecs.NewInstance(ctx, "instance-1", &ecs.InstanceArgs{
 		AvailabilityZone:        pulumi.String(az1),
 		ImageId:                 pulumi.String(fortigate622ImageId),
-		VswitchId:               vswitch.ID(),
+		VswitchId:               vswitchpub.ID(),
 		InternetMaxBandwidthOut: pulumi.Int(5),
 		InstanceType:            pulumi.String(myinstanceType),
 		SecurityGroups:          pulumi.StringArray{group.ID()},
 		InstanceName:            pulumi.String("myinstanceCreatedbyPulumi"),
 		InstanceChargeType:      pulumi.String("PostPaid"),
-		DryRun:                  pulumi.Bool(true),
+		DryRun:                  pulumi.Bool(false),
+		Status:			 pulumi.String("Stopped"),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	eniAtt1, err := vpc.NewNetworkInterfaceAttachment(ctx, "firstsecondaryeniattachtovswitchpri1", &vpc.NetworkInterfaceAttachmentArgs{
+		InstanceId:         myinstance.ID(),
+		NetworkInterfaceId: eni1.ID(),
 	})
 
 	if err != nil {
-		return nil,err
+		return nil, err
+	}
+
+	_, err = vpc.NewRouteEntry(ctx, "rtpri1defaultroutetofortigateeni1", &vpc.RouteEntryArgs{
+		DestinationCidrblock: pulumi.String("0.0.0.0/0"),
+		Name:                 pulumi.String("defaultroute"),
+		NexthopId:            eni1.ID(),
+		NexthopType:          pulumi.String("NetworkInterface"),
+		RouteTableId:         rtpri1.ID(),
+	})
+	if err != nil {
+		return nil, err
 	}
 
 	return &infrastructure{
 		vpcnetwork:        myvpc,
-		vswitch:           vswitch,
+		vswitchpub:        vswitchpub,
+		vswitchpri1:       vswitchpri1,
 		securitygroup:     group,
 		fortigateinstance: myinstance,
-		sgrule: sgrule1,
+		sgrule:            sgrule1,
+		eni:               eni1,
+		rtpri1:            rtpri1,
+		eniAtt:             eniAtt1,
 	}, nil
 }
 
@@ -113,11 +205,16 @@ func main() {
 		}
 
 		ctx.Export("vpcName", infra.vpcnetwork.ID())
-		ctx.Export("vswitchName", infra.vswitch.ID())
+		ctx.Export("vswitchpubName", infra.vswitchpub.ID())
+		ctx.Export("vswitchpri1Name", infra.vswitchpri1.ID())
 		ctx.Export("sgName", infra.securitygroup.ID())
 		ctx.Export("myinstance", infra.fortigateinstance.ID())
 		ctx.Export("myinstancepublicIP", infra.fortigateinstance.PublicIp)
-		ctx.Export("securtygrouprole",infra.sgrule.ID())
+		ctx.Export("securtygrouprole", infra.sgrule.ID())
+		ctx.Export("eni1", infra.eni.ID())
+		ctx.Export("eniattachment1",infra.eniAtt.ID())
+		ctx.Export("eniattachment-instanceID",infra.eniAtt.InstanceId)
+		ctx.Export("eniattachment-Interface",infra.eniAtt.NetworkInterfaceId)
 		return nil
 	})
 }
